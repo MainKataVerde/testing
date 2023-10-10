7a80a5ed6e806434125ff0dc965d0685d721c017
Oscar Mukeshimana Nzabarinda <101839576+MainKataVerde@users.noreply.github.com>
2023-10-10 18:44:51 +0200

    añadimos calculadora

---
+++ Calculadora/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double num12, num2, resultado;
+        
+        int hola = 12 ; 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer número: ");
+        num12= sc.nextDouble();
+        System.out.println("Ingresa el segundo número: ");
+        num2 = sc.nextDouble();
+        resultado = num12 + num2;
+
+        System.out.println("La suma de " + num12 + " y " + num2 + " es " + resultado + ".");
+        sc.close();
+
+    }
+}
